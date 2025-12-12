d43cd59f65d8e90e9a9f1537a5ff2cd49b09c646
CondesnsedMilk <achau103@student.hbuhsd.edu>
2025-12-12 22:29:14 +0000

    Done

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,10 +1,47 @@
-/*
+public class Scoreboard
+{
+    private String team1;
+    private String team2;
+    private String activeTeam;
+    private int point1;
+    private int point2;
 
-# Answer close reading 1 questions here
+    public Scoreboard(String team1, String team2)
+    {
+        this.team1 = team1;
+        this.team2 = team2;
+        activeTeam = team1;
+    }
 
+    public void recordPlay(int points)
+    {
+        if(activeTeam.equals(team1))
+        {
+            if(points > 0)
+            {
+                point1 += points;
+            }
+            else
+            {
+                activeTeam = team2;
+            }
+        }
+        else
+        {
+            if(points > 0)
+            {
+                point2 += points;
+            }
+            else
+            {
+                activeTeam = team1;
+            }
+        }
 
-# Answer close reading 2 questions here
+    }
 
-*/
-
-// Write the class below
+    public String getScore()
+    {
+        return String.format("%d-%d-%s", point1, point2, activeTeam);
+    }
+}
